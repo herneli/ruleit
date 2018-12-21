cf3b5866dbbbc56d2009887f34946546c5046d67
Jordi Hernandez <jordi.hernandez@roche.com>
2018-12-21 12:49:54 +0100

    New change

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const defaultAwesomeFunction = (name) => {
-  const returnStr = `I am the Default Awesome Function, fellow comrade! - ${name}`;
+const defaultAwesomeFunction = name => {
+  const returnStr = `I am the Default Awesome Function, fellow comrade 2! - ${name}`;
   return returnStr;
 };
 
-const awesomeFunction = () => 'I am just an Awesome Function';
+const awesomeFunction = () => "I am just an Awesome Function";
 
 export default defaultAwesomeFunction;
 
